0a54c0659b87dcf6b8cb08c42083c9552313048a
sbhongir <sankethbhongir06@gmail.com>
2019-01-10 16:37:58 -0500

    Added insertion feature in editor activity

--- app/src/main/java/com/example/sanketh/storekeep/EditorActivity.java
+++ app/src/main/java/com/example/sanketh/storekeep/EditorActivity.java
@@ -1,11 +1,17 @@
 package com.example.sanketh.storekeep;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.sanketh.storekeep.ProductData.ProductContract;
+import com.example.sanketh.storekeep.ProductData.ProductContract.ProductEntry;
 
 /**
  * Allows user to create a new product or edit an existing one.
@@ -34,6 +40,49 @@ public class EditorActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Get user input from editor and save new pet into database.
+     */
+
+    private void insertPet() {
+        // Read from input fields
+        // Use trim to eliminate leading or trailing white space
+        String productName = mProductEditText.getText().toString().trim();
+        int productPrice = Integer.parseInt(mPriceEditText.getText().toString().trim());
+        int productQuantity = Integer.parseInt(mQuantityEditText.getText().toString().trim());
+        String supplierName = mSupplierEditText.getText().toString().trim();
+        String supplierPhone = mPhoneEditText.getText().toString().trim();
+
+
+
+        // Create a ContentValues object where column names are the keys,
+        // and Toto's pet attributes are the values.
+        ContentValues values = new ContentValues();
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, productName);
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, productPrice);
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, productQuantity);
+        values.put(ProductEntry.COLUMN_SUPPLIER_NAME, supplierName);
+        values.put(ProductEntry.COLUMN_SUPPLIER_PHONE_NUMBER, supplierPhone);
+
+        // Insert a new pet into the provider, returning the content URI for the new pet.
+        Uri newUri = getContentResolver().insert(ProductEntry.CONTENT_URI, values);
+
+
+        // Show a toast message depending on whether or not the insertion was successful
+        if (newUri == null) {
+            // If the new content URI is null, then there was an error with insertion.
+            Toast.makeText(this, getString(R.string.editor_insert_product_failed),
+
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            // Otherwise, the insertion was successful and we can display a toast.
+            Toast.makeText(this, getString(R.string.editor_insert_product_successful),
+                    Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // User clicked on a menu option in the app bar overflow menu
@@ -41,6 +90,7 @@ public class EditorActivity extends AppCompatActivity {
             // Respond to a click on the "Save" menu option
             case R.id.action_save:
                 // Save product to database
+                insertPet();
 
                 // Exit activity
                 finish();
